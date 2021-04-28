e37ef34695efa1c3e040f4fe6daf67ad33adf98a
Cipolotti Manuel <manuel.cipolotti@capgemini.com>
2021-04-29 00:18:55 +0200

    Add search field and view structure

--- RedditImage/ViewController.swift
+++ RedditImage/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchText.addRightImage(imageName: "search-plus", arrow: false)
+        searchText.addPadding(.left(8))
     }
 
 
