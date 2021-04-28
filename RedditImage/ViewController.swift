f2d8598fe52832f73e2781f23e60164a2933c2ab
Cipolotti Manuel <manuel.cipolotti@capgemini.com>
2021-04-28 23:06:58 +0200

    Initial commit

---
+++ RedditImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditImage
+//
+//  Created by Manuel Cipolotti Beta on 28/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
